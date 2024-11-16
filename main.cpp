8b3c4cbf605edb1a64de2d006f7d4b58b3e825b2
vasya000 <vasya@gmail.com>
2024-11-16 15:23:04 +0200

    aa

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int salary = 45;
+	int max_salary = 50;
+	int hours;
+	int overtime;
+	cout << "Enter the number of hours worked";
+	cin >> hours;
+	int result_salary = (hours <= 40) ? salary * hours : salary * 40 + (hours - 40) * max_salary;
+	cout << " the salary is: " << result_salary << endl;
+	return 0;
+}
